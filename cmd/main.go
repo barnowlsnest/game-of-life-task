93ecd36f77e835c710c569fc598e76f7967b36c0
Dzianis Shlychkou <dzianis.shlychkou@say.games>
2025-06-05 20:10:33 +0200

    docs: add comments to the main.go

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,13 @@ const (
 	cols = 25 // dimension Y
 )
 
+/**
+ * Potential improvements of the task:
+ * - I would search for more optimal way how to work with the grid state: right now every tick performs O(n^2)
+ * - Maybe it makes sense to handle states in segments not the whole grid as we do a lot of redundant CPU time.
+ * - Maybe we can have the hashtable where the key is each cell and values are set of neighbours - it probably will provide +- O(1) access and O(n) traversal
+ * - What to do when we reach the edge of the boundary. Right now app will consume resource and do nothing - detect and quite or try to reset state?
+ */
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	done := make(chan struct{})
